32b3a774cae88405875c7fc10f946cd8a54fd49d
Billy Wong <bwong5@stuy.edu>
2017-02-04 16:59:15 -0500

    get List of dicts from csvs done

--- db_builder.py
+++ db_builder.py
@@ -1,4 +1,36 @@
 from pymongo import MongoClient
+import csv
+
+
+def getPeepsDict():
+    peepsFile = open("data/peeps.csv")
+    peepReader = csv.DictReader(peepsFile)
+    peeps = []
+    for peep in peepReader:
+        pInfo = {}
+        pInfo["name"] = peep["name"]
+        pInfo["age"] = peep["age"]
+        pInfo["id"] = peep["id"]
+        peeps.append(pInfo)
+    peepsFile.close()
+    return peeps
+
+
+
+def getCoursesDict():
+    coursesFile = open("data/courses.csv")
+    courseReader = csv.DictReader(coursesFile)
+    courses = []
+    for course in courseReader:
+        pInfo = {}
+        pInfo["code"] = course["code"]
+        pInfo["mark"] = course["mark"]
+        pInfo["id"] = course["id"]
+        courses.append(pInfo)
+    coursesFile.close()
+    return courses
+
+
 
 if __name__ == "__main__":
     c = MongoClient('lisa.stuy.edu', 27017)
