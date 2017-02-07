d02abeffd5a6ac3b4d615f035e7f274335eb55a8
wxiang54 <wxiang@stuy.edu>
2017-02-07 12:37:21 -0500

    adfawda

---
+++ teacher_builder.py
@@ -0,0 +1,44 @@
+from pymongo import MongoClient
+from collections import defaultdict
+from pprint import pprint
+import urllib2
+import csv
+
+def getTeachersDicts():
+    teachersFile = open("data/teachers.csv")
+    teachersReader = csv.DictReader(teachersFile)
+    teachers = []
+    for teacher in teacherReader:
+        pInfo = defaultdict(list)
+        pInfo["code"] = teacher["code"]
+        pInfo["teacher"] = teacher["teacher"]
+        pInfo["period"] = teacher["period"]
+        teachers.append(pInfo)
+    teachersFile.close()
+    return teachers
+
+
+
+def isLocalConnection():
+    try:
+        urllib2.urlopen('http://lisa.stuy.edu', timeout=1)
+        return True
+    except urllib2.URLError as err: 
+        return False
+
+
+if __name__ == "__main__":
+    allTeachersInfo = getTeachersDicts()
+    pprint(allTeachersInfo)
+
+    if isLocalConnection():
+        c = MongoClient('lisa.stuy.edu', 27017)
+        db = c['pokeMONGO_champions']
+        db.teachers.drop()
+        for teacher in allTeachersInfo:
+            studentsInCourse = db.students.find( {"courses.code" : teacher.code} )
+            for student in studentsInCourse:
+                pass #CHANGE THIS
+    else:
+        print "Not in Lisa :( ssh there first!"
+    print "Done."
